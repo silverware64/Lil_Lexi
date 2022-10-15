11c7a2e4b8fe07cc3d28a5334cd5fb63a40de45f
Nicholas Bell <macbell@protonmail.com>
2022-10-15 00:57:44 -0700

    cold brew nitro XV

---
+++ src/lillexi/Glyph.java
@@ -0,0 +1,33 @@
+package lillexi;
+/**
+ * Author: Tobin Nickels
+ * Date: October 14, 2022
+ *
+ * Purpose:
+ * Abstract class representing graphic on a text document.
+ *
+ *
+ */
+
+public abstract class Glyph {
+
+    protected Window window;
+
+    protected int width;
+    protected int height;
+
+    protected int x;
+    protected int y;
+
+    public int getWidth(){return width;}
+    public int getHeight(){return height;}
+
+    /**
+     * Used to update the glyph with current coordinates.
+     *
+     * @param x
+     * @param y
+     */
+    public abstract void draw(int x, int y);
+
+}
